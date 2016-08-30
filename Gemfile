83654f164d46e3776288ee19d53dd3de6f0349fd
dhilipsiva <dhilipsiva@gmail.com>
2016-08-30 15:28:31 +0530

    Downgrade to stable neat

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sass', '~> 3.4'
 gem 'bourbon', '~> 5.0.0.beta.6'
-gem 'neat', '~> 2.0.0.beta.1'
+gem 'neat', '~> 1.8'
 gem 'bitters', '~> 1.4'
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# setup.sh
+# Copyright (C) 2016 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+bundle install
+bundle exec bourbon install
+bundle exec neat install
