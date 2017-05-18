02cd5f4c6c1d5fbd016897cf47adcecafacca7e6
dhilipsiva <dhilipsiva@gmail.com>
2017-05-18 19:16:47 +0530

    Added a deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# deploy.sh
+# Copyright (C) 2017 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+npm version --minor
+git push origin --tags
